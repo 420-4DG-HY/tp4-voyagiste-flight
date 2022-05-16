e71845c34c03fb2347365e124ea75dec3f6d1e49
MainKarl <karlmainville26@gmail.com>
2022-05-16 12:36:45 -0400

    Merge logiquement fonctionnel

--- Voyagiste/CarAPI/Program.cs
+++ Voyagiste/CarAPI/Program.cs
@@ -4,7 +4,6 @@ global using CarDAL;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
